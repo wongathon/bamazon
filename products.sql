d6b6ca27a5e35c07a54adc031a3c92cb37d63ca0
Andrew Wong <andrew.wong@andrew-wong-compy.local>
2017-04-28 17:23:25 -0400

    sql add

---
+++ products.sql
@@ -0,0 +1,26 @@
+-- CREATE DATABASE Bamazon;
+-- USE Bamazon;
+
+-- CREATE table products (
+-- 	item_id INTEGER(5) AUTO_INCREMENT NOT NULL,
+--     product_name VARCHAR(30) NOT NULL,
+--     department_name VARCHAR(30) NOT NULL,
+--     price DECIMAL(5,2) NOT NULL,
+--     stock_quantity VARCHAR(4) NOT NULL,
+--     PRIMARY KEY(item_id)
+--     );
+--     
+-- INSERT INTO products (product_name, department_name, price, stock_quantity)
+-- VALUES
+-- ("Brownie Bites", "Food & Snacks", "9.46", "900"),
+-- ("Cookie Crisp", "Food & Snacks", "4.95", "300"),
+-- ("Fritos", "Food & Snacks", "8.50", "500"),
+-- ("Jeans", "Clothing", "45.84", "50"),
+-- ("Shorts", "Clothing", "38.12", "40"),
+-- ("Polos", "Clothing", "39.99", "68"),
+-- ("Baseball Caps", "Clothing", "28.23", "30"),
+-- ("Magic Bullet", "Appliances", "55.00", "40"),
+-- ("Dyson Vacuum", "Appliances", "168.25", "50"),
+-- ("Regular ol' Plumbus", "Appliances", "800.99", "14"); 
+
+SELECT * FROM products;
